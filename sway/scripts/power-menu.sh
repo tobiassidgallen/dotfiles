d78c1b884ba33c32da557a711fbd02b5e4604dda
tobiassidgallen <tobiassidgallen@gmail.com>
2025-08-31 19:54:03 +0800

    Update power-menu.sh

--- sway/scripts/power-menu.sh
+++ sway/scripts/power-menu.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 choice=$(echo -e "Lock\nSleep\nShutdown\nRestart" | wofi --dmenu --prompt "Power:")
+
+# Exit if no choice made (user canceled)
+[ -z "$choice" ] && exit 0
+
 case "$choice" in
     Lock) swaylock -f -c 000000 -i ~/Pictures/wallpapers/lockscreen.jpg --show-failed-attempts --show-keyboard-layout --indicator-caps-lock ;;
     Sleep) systemctl suspend ;;
     Shutdown) systemctl poweroff ;;
     Restart) systemctl reboot ;;
+    *) exit 1 ;;  # Handle unexpected input
 esac
